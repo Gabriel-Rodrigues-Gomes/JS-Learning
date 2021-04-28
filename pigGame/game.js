564430c90c6a5aa99b0a4afd4af573411c5a20b8
Gabriel <grgprofissional@gmail.com>
2021-04-28 13:30:26 -0300

    fixed numbers not resetting

--- pigGame/game.js
+++ pigGame/game.js
@@ -1,58 +1,55 @@
 
-var scores,roundScore,activePlayer,dice;
+var roundScore,activePlayer,dice;
 
 p0Panel = document.querySelector('.player-0-panel')
 p1Panel = document.querySelector('.player-1-panel')
-
-score0 = 0;
-score1 = 0;
-
 roundScore = 0;
-
 activePlayer = 0;
 
 
 //new game function
-document.querySelector('.btn-new').addEventListener('click',function () {
-    document.querySelector('#score-0').innerHTML = 0;
-    document.querySelector('#score-1').innerHTML = 0;
-    document.querySelector('#current-0').innerHTML = 0;
-    document.querySelector('#current-1').innerHTML = 0;
+document.querySelector('.btn-new').addEventListener('click',newGame);
+
+
+function newGame() {
+    document.querySelector('#score-0').textContent = 0;
+    document.querySelector('#score-1').textContent = 0;
+    
+    document.querySelector('#current-0').textContent = 0;
+    document.querySelector('#current-1').textContent = 0;
+    
     score0 = 0;
     score1 = 0;
+    
     p0Panel.className = 'player-0-panel';
     p1Panel.className = 'player-1-panel';
-});
 
-
+    document.querySelector('#dice-0').style.display = 'none';
+    activePlayer = 0;
+    roundScore = 0;
+}
 
 //'roll dice' button
 document.querySelector('.btn-roll').addEventListener('click',function() { 
-    var dice = Math.floor( Math.random() * 6) + 1; 
-    if(activePlayer == 0) {
-        var diceDOM = document.querySelector('#dice-0');
-        diceDOM.src = 'dice-' + dice + '.png';
+    dice = Math.floor( Math.random() * 6) + 1; 
+    document.querySelector('#dice-0').src = 'dice-' + dice + '.png';
+    document.querySelector('#dice-0').style.display = 'block';
 
-        score0+=dice;
-        document.querySelector('#score-0').innerHTML = score0;
+    if(dice !== 1){
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }else{
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
 
-        activePlayer = 1;
-        p1Panel.className = 'player-1-panel active';
-
-        p0Panel.className = 'player-0-panel';
-
-    }
-    else if(activePlayer == 1) {
-        var diceDOM = document.querySelector('#dice-0');
-        diceDOM.src = 'dice-' + dice + '.png';
-
-        score1+=dice;
-        document.querySelector('#score-1').innerHTML = score1;
-
-        activePlayer = 0;
-        p0Panel.className = 'player-0-panel active';
-
-        p1Panel.className = 'player-1-panel';
+    
+        document.getElementById('current-0').textContent = 0;
+        document.getElementById('current-1').textContent = 0;
 
     }
-} );
+
+
+});
+//hold button
+//document.querySelector('.btn-hold').addEventListener('click',);
